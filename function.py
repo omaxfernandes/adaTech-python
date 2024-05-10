4c296668b7ca362c90e2d59a88a47132cbe4fd9d
omaxfernandes <max.sfernandes@outlook.com>
2024-05-10 14:07:28 -0300

    Declaration of functions in python

---
+++ function.py
@@ -0,0 +1,16 @@
+# Create function using sitax "def"
+
+def welcome(name,course):
+    print(f"Welcome, {name}")
+    print(f"You are on couser, {course}")
+   
+
+welcome("max", "python")   
+
+# Function with return value
+
+def sum(a, b):
+    return a + b
+
+result = sum(5,7)
+print(result)
