cea7c191f4f10b68f2bee35e1a4f10570054eef3
omaxfernandes <max.sfernandes@outlook.com>
2024-05-10 13:52:53 -0300

    Function for list in python

---
+++ functionForList.py
@@ -0,0 +1,22 @@
+# Function for list
+
+list = [1, 2, 3, 4, 5]
+
+# len
+print("len, count itens of list")
+print(len(list))
+print("")
+
+# sum
+print("sum, sum all itens of list")
+print(sum(list))
+print("")
+
+# max
+print("max, max value of list")
+print(max(list))
+print("")
+
+# min
+print("min, min value of list")
+print(min(list))
