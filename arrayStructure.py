3dadee979d9109a65247d1ae06c1f7e3beb34d10
omaxfernandes <max.sfernandes@outlook.com>
2024-05-10 13:22:28 -0300

    Array Structure in python

---
+++ arrayStructure.py
@@ -0,0 +1,29 @@
+# Declaration of list using variable
+
+note1 = 9.8
+note2 = 8.9
+note3 = 7.8
+
+# Create array 
+notes = [9.8, 8.9, 7.8]
+
+array = []
+array = list()
+user = [26, "John", "XXXXXXXXXXXXXXXXXX", 1.88, True]
+
+# indexing and slicing
+print(user[0])
+print(user[1])
+print(user[2])
+print(user[3])
+print(user[4])
+# print(user[5]) error
+
+print ("slicing")
+print(user[0:2])
+
+numbers = [1, 2, 3, 4, 5]
+print(numbers[2:5:2])
+
+for i in range(0, len(numbers)):
+    print(numbers[i])
