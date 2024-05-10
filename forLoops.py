c4204a4f1248c2cc1da3ae92fd3ecdcf561a22a0
omaxfernandes <max.sfernandes@outlook.com>
2024-05-10 13:08:21 -0300

    For loops in python

---
+++ forLoops.py
@@ -0,0 +1,15 @@
+# for loop 
+
+""""
+    for variable in range(5):
+    print(variable)
+"""
+
+""""
+    for variable in range(5, 10):
+    print(variable)
+"""
+
+
+for variable in range(0, 10, 2):
+    print(variable)
