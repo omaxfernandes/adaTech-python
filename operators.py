4cd39447e7f30f31aa394855cee136059fd64727
omaxfernandes <max.sfernandes@outlook.com>
2024-05-10 11:47:25 -0300

    arithmetic operators and bool operators

---
+++ operators.py
@@ -0,0 +1,39 @@
+# Arithmetic operators
+"""
+    sum = +
+    subritraction = - 
+    mutiply = *
+    division = /
+    remainder of division = %
+    potentiation = **
+    intiger division = //
+"""
+print("Arithmetic operators:")
+print("")
+
+print("sum: 4 + 4 =",4 + 4) 
+print("subtraction: 4 - 2 =", 4 - 2)
+print("mutiply: 4 * 3 =", 4 * 3)
+print("division: 25 / 5 =", 25 / 5)
+print("remainder of division: 21 % 4 =", 21 % 4)
+print("potentiation: 2 ** 3 =", 2 ** 3)
+print("intiger division: 25 // 5 =", 25 // 5)
+print("")
+
+
+# Boolean operators
+print("Boolean operators:")
+print("")
+
+age1 = 25
+age2 = 45
+height1 = 1.88
+height2 = 1.65
+
+print("age1 > age2:", age1 > age2) # False
+print("age1 <= age2:", age1 <= age2) # True
+print("height1 > height2:", height1 > height2) # True
+print("height1 >= height2:", height1 >= height2) # True
+
+print("Python" == "python") # False
+print("Babana" != "apple") # True
