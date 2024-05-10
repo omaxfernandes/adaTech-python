161d9022586e28b58ae6044182b091416f7bb442
omaxfernandes <max.sfernandes@outlook.com>
2024-05-10 12:51:20 -0300

    While Loops

---
+++ loopWhile.py
@@ -0,0 +1,23 @@
+#  loop while,  enquanto uma condição for diferente, o loop continua.
+
+print("loop while")
+
+num = 15 
+numUser = int(input("Enter a number: "))
+
+while numUser != num:
+    print("Wrong number")
+    numUser = int(input("Enter a number: "))
+
+print("You got it right")
+
+
+
+
+
+
+
+
+
+
+#  loop for,  para cada item em uma lista, o loop continua.
