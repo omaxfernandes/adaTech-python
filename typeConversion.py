d5e5b47cf5fdf5496e4da13029a08cb991473033
omaxfernandes <max.sfernandes@outlook.com>
2024-05-10 11:59:20 -0300

    Type conversion in python

---
+++ typeConversion.py
@@ -0,0 +1,11 @@
+# Type conversion use functions int(), float(), str() and bool().
+
+print("string for intiger")
+age = "23"
+print(type(age))
+
+intigerAge = int(age)
+print(type(intigerAge))
+print("")
+
+
